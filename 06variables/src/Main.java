52563ed2cc615ec0fd6e06aab5757fe97a33db12
Mustafa Atmaca <m.atmaca.official@hotmail.com>
2023-02-04 18:53:11 +0300

    variables commit

---
+++ 06variables/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// **tc kimlik numarası ile matematiksel
+		// işlem yapılmadığı için String
+		// primative data types boolean,char,byte,short,long,float,int,,double
+		double sayi = 12;
+		sayi = -129;
+
+		char karakter = 'A';
+
+		boolean dogruMu = false;
+	}
+
+}
