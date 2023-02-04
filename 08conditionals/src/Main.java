f8e0ad28604474a7ecf42a58af545ef791dd8c7d
Mustafa Atmaca <m.atmaca.official@hotmail.com>
2023-02-04 19:10:55 +0300

    conditionals commit

---
+++ 08conditionals/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// if if olsaydı ikiside çalışırdı
+		// if-else if olduğunda sadece biri çalışır.
+		int sayi = 20;
+		if (sayi < 20) {
+			System.out.println("Sayı 20'den küçüktür.");
+		} else if (sayi == 20) {
+			System.out.println("Sayı 20'ye eşittir");
+		} else {
+			System.out.println("Sayı 20'den büyüktür. ");
+		}
+	}
+
+}
