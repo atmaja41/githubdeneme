7813b448cea4a06d49ba70e25bbfc557086a3353
Mustafa Atmaca <m.atmaca.official@hotmail.com>
2023-02-04 21:25:38 +0300

    for loop commit

--- 11loopdemo/src/Main.java
+++ 11loopdemo/src/Main.java
@@ -8,21 +8,7 @@ public class Main {
 			System.out.println(i);
 		}
 		System.out.println("For Döngüsü Bitti");
-//While
-		int i = 2;
-		while (i < 10) {
-			System.out.println(i);
-			i += 2;
-		}
-		System.out.println("Do-While Döngüsü Bitti");
-//Do-While
-		int j = 2;     
-		/* kod satır satır ilerlediği için şartı sağlamasa bile bir kere
-		  çalışır.Şart sağlandığında normal while gibi çalışır.*/
-		do {
-			System.out.println(j);
-			j += 2;
-		} while (j < 10);
+
 
 	}
 
