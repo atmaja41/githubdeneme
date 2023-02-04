b0e273a2602c873d8c68ea78e44de32f519a5d2c
Mustafa Atmaca <m.atmaca.official@hotmail.com>
2023-02-04 19:15:32 +0300

    RecapDemo1 commit

---
+++ 09Recapdemo1/src/Main.java
@@ -0,0 +1,31 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int sayi1 = 24;
+		int sayi2 = 26;
+		int sayi3 = 2;
+		int enBuyuk = sayi1;
+		if (enBuyuk < sayi2) {
+			enBuyuk = sayi2;
+		}
+		if (enBuyuk < sayi3) {
+			enBuyuk = sayi3;
+		}
+		System.out.println("En Büyük = " + enBuyuk);
+		/*
+		 * if (sayi1 == sayi2 && sayi2 == sayi3) {
+		 * System.out.println("Tüm sayılar eşittir"); } else if (sayi1 > sayi2 && sayi1
+		 * > sayi3) { System.out.println(sayi1 + " büyüktür"); } else if (sayi2 > sayi1
+		 * && sayi2 > sayi3) { System.out.println(sayi2 + " büyüktür"); } else {
+		 * System.out.println(sayi3 + " büyüktür"); } if (sayi1 == sayi2) {
+		 * System.out.println("sayi1 sayi2'e eşittir"); } else if (sayi1 == sayi3) {
+		 * System.out.println("sayi1 sayi3'e eşittir"); }else if(sayi2==sayi3){
+		 * System.out.println("sayi2 sayi3'e eşittir");}
+		 */
+
+	}
+
+}
