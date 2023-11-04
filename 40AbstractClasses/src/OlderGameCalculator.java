6b7f2cc7f4587c9ef6531467af582caf8e395228
atmcgit <m.atmaca.official@hotmail.com>
2023-11-04 18:27:19 +0300

    abstract classes added

---
+++ 40AbstractClasses/src/OlderGameCalculator.java
@@ -0,0 +1,11 @@
+
+public class OlderGameCalculator extends GameCalculator{
+
+	@Override
+	public void hesapla() {
+		// TODO Auto-generated method stub
+		System.out.println("Puanınız : 85");
+		
+	}
+	
+}
---
+++ 40AbstractClasses/src/WomanGameCalculator.java
@@ -0,0 +1,12 @@
+
+public class WomanGameCalculator extends GameCalculator {
+
+	@Override
+	public void hesapla() {
+		// TODO Auto-generated method stub
+		System.out.println("Puanınız : 95");
+		
+	}
+
+
+}
